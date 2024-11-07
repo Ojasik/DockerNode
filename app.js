f726f75714b9e8372c949f7e73d4aad019fb6ef3
Marks <meshkis.mark@gmail.com>
2024-11-07 15:28:44 +0200

    f

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello, Jenkins & Docker!');
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0",() => {
   console.log(`App listening at http://localhost:${port}`);
 });
